9bcc9ac341e611d0ab13409634b8dd03baff91e2
ViAn-Monsieur <tranvian.dev@gmail.com>
2026-07-14 12:53:52 +0700

    them config

--- backend/SmartDriveIoT/src/main/java/com/example/smartdriveiot/config/SecurityConfig.java
+++ backend/SmartDriveIoT/src/main/java/com/example/smartdriveiot/config/SecurityConfig.java
@@ -13,6 +13,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -26,7 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) // Tắt CSRF vì hệ thống chạy Stateless REST API
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors->cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         // Sử dụng AntPathRequestMatcher để Spring Boot 3 nhận diện chính xác
                         .requestMatchers(new AntPathRequestMatcher("/api/auth/**")).permitAll()
@@ -56,7 +62,19 @@ public class SecurityConfig {
 
         return http.build();
     }
+    // Bean cấu hình chi tiết CORS dành riêng cho Spring Security
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://ten-mien-frontend-cua-ban.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setAllowCredentials(true);
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
